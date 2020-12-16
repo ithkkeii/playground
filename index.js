393bc2cab942de8fd0e19974887bd14f425d8749
ithkkeii <ithkkeii@gmail.com>
2020-12-16 23:41:27 +0700

    .

--- index.js
+++ index.js
@@ -2,5 +2,10 @@ console.log("hotfix");
 
 const sum = (a, b) => {
   console.log("hello new world");
+  console.log("hello new world");
+  console.log("hello new world");
+  console.log("hello new world");
+  console.log("hello new world");
+  console.log("hello new world");
   return a * b;
 };
