55f2a576a98483d79755a2f7b62524682268942b
ithkkeii <ithkkeii@gmail.com>
2021-01-07 21:29:27 +0700

    Solve conflict.

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -8,3 +8,7 @@ export const minus = () => {
   console.log(3);
   console.log(2);
 };
+export const minus = () => {};	export const sum = () => {
+  export const plus = () => {};	  console.log(1);
+  export const mul = () => {};	  console.log(2);
+  export const add = () => {};
