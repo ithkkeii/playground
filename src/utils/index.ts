d99199e9a75b41805cbf5ef6b23078385a813e36
ithkkeii <ithkkeii@gmail.com>
2021-01-07 21:33:22 +0700

    Create conflict

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,14 +1,16 @@
-export const sum = () => {
-  console.log(1);
-  console.log(2);
+console.log(1);
+console.log(3);
+console.log(2);
+};
+export const minus = () => {};	export const sum = () => {
+export const plus = () => {};	  console.log(1);
   console.log(4);
 };
 export const minus = () => {
-  console.log(1);
-  console.log(3);
-  console.log(2);
-};
-export const minus = () => {};	export const sum = () => {
-  export const plus = () => {};	  console.log(1);
   export const mul = () => {};	  console.log(2);
   export const add = () => {};
+  
+  export const sum = () => {
+    console.log(1);
+    console.log(2);
+  
