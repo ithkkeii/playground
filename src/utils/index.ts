663e6e59e6725fb65187218a5e9cd5b57e9cca75
ithkkeii <ithkkeii@gmail.com>
2021-01-07 21:38:35 +0700

    Final squash init
    Change 1
    
    Change 2
    
    Change 3
    
    1
    
    2
    
    3

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,19 +1,30 @@
 t plus = () => {};	  console.log(1);
       console.log(4);
     };
-    export const minus = () => {
-      export const mul = () => {};	  console.log(2);
-      export const add = () => {};
-      
-      export const sum = () => {
-        console.log(1);
-        console.log(2);
-        console.log(1);
-        console.log(3);
-        console.log(2);
-        };
-        export const minus = () => {};	export const sum = () => {
-        export const plus = () => {};	  c
+
+console.log("222");
+console.log("222");
+console.log("222");
+console.log("222");
+console.log("222");
+console.log("222");
+console.log("222");
+console.log(1);
+console.log(2);
+console.log(1);
+console.log(3);
+console.log(2);
+console.log(1);
+console.log(3);
+console.log(2);
+console.log(1);
+console.log(3);
+console.log(2);
+export const minus = () => {
+  export const mul = () => {};	  console.log(2);
+  export const add = () => {};
+  
+  export const sum = () => {
     console.log(1);
     console.log(2);
     console.log(1);
@@ -21,14 +32,36 @@ t plus = () => {};	  console.log(1);
     console.log(2);
     };
     export const minus = () => {};	export const sum = () => {
-    export consonsole.log(1);
-          console.log(4);
-        };
-        export const minus = () => {
-          export const mul = () => {};	  console.log(2);
-          export const add = () => {};
+    export const plus = () => {};	  c
+console.log(1);
+console.log(2);
+console.log(1);
+console.log(3);
+console.log(2);
+};
+export const minus = () => {};	export const sum = () => {
+export consonsole.log(1);
+      console.log(4);
+    };
+    export const minus = () => {
+      export const mul = () => {};	  console.log(2);
+      export const add = () => {};
+      
+      export const sum = () => {
+        console.log(1);
+        console.log(2);
           
-          export const sum = () => {
-            console.log(1);
-            console.log(2);
-              
+console.log(1);
+console.log("222");
+console.log("222");
+console.log(3);
+console.log(3);
+console.log(3);
+console.log(3);
+console.log(3);
+console.log("222");
+console.log("222");
+console.log("222");
+console.log(3);
+console.log(3);
+console.log(3);
